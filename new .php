bcb92c00660da8c429518fefa19704b9724c4450
swethasenthil2662 <swethasenthil266@gmail.com>
2023-06-01 23:15:09 +0530

    first commit

---
+++ new .php
@@ -0,0 +1,48 @@
+<?php
+
+echo ("hello world");
+print("hello");
+$d=4;
+$f=10;
+echo("$d.$f");
+echo("<br>");
+
+for($i=0;$i<10;$i++)
+{
+    echo("<br>2*");
+
+    echo("<br>2*");
+    echo($i+1);
+    echo("=");
+    echo(2*($i+1));
+
+
+}
+?>
+
+<!--homework-->
+
+<!DOCTYPE html>
+<html lang="en">
+<body>
+
+<?php  
+
+$num = 9;  
+
+for($i=1; $i<=10; $i++)   
+{
+$product = $i*$num;
+
+echo "$num * $i = $product" ;   
+
+echo '<br>';	
+
+} 
+
+?> 
+</body>
+</html>
+
+
+
